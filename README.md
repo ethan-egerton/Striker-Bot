def43be28284b8e36d2c346677ebb3ece3a7dacb
Ethan Egerton <38867954+ethan-egerton@users.noreply.github.com>
2021-03-30 13:39:15 +0100

    Added striker bot code
    Changed readme, added node package and added bot code

---
+++ README.md
@@ -0,0 +1,10 @@
+# Striker Discord Bot
+
+This Discord application streams data from the twitter user [@Striker729](https://twitter.com/Striker729) and displays the link to new tweets to a private discord.
+
+## Requirements
+
+```bash
+npm i discord.js
+npm i twit
+```
---
+++ striker-bot.js
@@ -0,0 +1,24 @@
+var Twit = require('twit');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var T = new Twit({
+  consumer_key:         'consumerkey',
+  consumer_secret:      'consumersecret',
+  access_token:         'accesstoken',
+  access_token_secret:  'accesstokensecret',
+})
+
+var stream = T.stream('statuses/filter', { follow: ['1231972210769907717'] });
+
+stream.on('tweet', (tweet) => {
+  console.log("tweet detected")
+  var tweetID = tweet['id_str']
+  var tweetTime = tweet['created_at']
+  client.channels.cache.get('544998477193674791').send(`Striker just tweeted:\n${tweetTime}\n\nhttps://twitter.com/i/status/${tweetID}`)
+});
+
+
+console.log('Online');
+
+client.login('ODI2MjU3NzE4MjY3NDc4MDI3.YGJ2dw._ZE2nEeB77EgHhEYBt-yGCxmbs4');
